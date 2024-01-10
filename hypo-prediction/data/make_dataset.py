8d8ec1cb78db16b12f8dd14d5b381ef00ecb1138
TheGoldenChicken <Karl Meisner>
2024-01-10 16:51:05 +0100

    completed word on make_data

--- hypo-prediction/data/make_dataset.py
+++ hypo-prediction/data/make_dataset.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import torch
-from datasets import load_dataset
 import os
-import torchtext
-from transformers import AutoTokenizer
 import itertools
+import pandas as pd
+from torch import load, save
+from datasets import load_dataset
+from torchtext.data import get_tokenizer
+from transformers import AutoTokenizer
 
 def das_huggingface_pirater():
     """Pirates the huggingface library
@@ -22,12 +22,15 @@ def das_huggingface_pirater():
 def dataset_preprocessor(data_path):
 
     data = pd.read_csv(f'data/raw/{data_path}.csv')
-    tokenizer = torchtext.data.get_tokenizer('basic_english')
+    tokenizer = get_tokenizer('basic_english')
 
     data[['premise', 'hypothesis']] = data[['premise', 'hypothesis']].astype(str).map(tokenizer)
 
-    combined_with_delim = [i + ['[CLS]'] + r +  ['[EOS]'] for i, r in zip(data['premise'], data['hypothesis'])]
+    combined_with_delim = [['[CLS]'] + i + ['[SEP]'] + r + ['[SEP]']  for i, r in zip(data['premise'], data['hypothesis'])]
 
+    save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
+    save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
+    save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
 
     # if use_vocab:
     #     # vocab = torchtext.vocab.build_vocab_from_iterator(combined_with_delim, specials=['[UNK]', '[CLS]', '[EOS]'])
@@ -38,23 +41,23 @@ def dataset_preprocessor(data_path):
         # return vocab, combined_with_delim
     
 
-    torch.save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
-    torch.save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
-    torch.save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
     
 
-def make_extended_vocab(data, model_name='bert'):
+def make_extended_vocab(data_path, model_name='bert'):
     """
     Assume 'data' is file loadable by pytorch that is in some way a list of lists of tokenized words  
     """
 
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    data = torch.load(data)
-    tokenizer.add_tokens
-    new_words = set(itertools.chain(*data))
+    data = load(data_path)
+    new_words = list(set(itertools.chain(*data)))
+    tokenizer.add_tokens(new_words)
+
+    save(tokenizer, f'models/{model_name}_extended_tokenizer.pt')
+
 
 if __name__ == '__main__':
-
+ 
     # All datasets in the huggingface multi_nli library
     sets = ['train', 'validation_matched', 'validation_mismatched']
     if not all([os.path.exists(f'./data/raw/{i}.csv') for i in sets]):
@@ -64,3 +67,8 @@ if __name__ == '__main__':
     print('Processing data...')
     for name in sets:
         dataset_preprocessor(name)
+
+    model_name = 'bert-base-uncased'
+    data_path = './data/processed/train-input.pt'
+    print(f'Extending tokenizer for {model_name} with words from {data_path}')
+    make_extended_vocab(data_path=data_path, model_name=model_name)
