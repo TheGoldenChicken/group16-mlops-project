b6ff89af6a958f88edacab34dbdc1ed669118695
TheGoldenChicken <Karl Meisner>
2024-01-15 08:15:37 +0100

    just add all stuff

--- hypo_prediction/data/make_dataset.py
+++ hypo_prediction/data/make_dataset.py
@@ -7,7 +7,7 @@ from torchtext.data import get_tokenizer
 from transformers import AutoTokenizer
 
 
-def das_huggingface_pirater():
+def das_huggingface_pirater(save_format):
     """Pirates the huggingface library
     Yeah no it doesn't really pirate it, but you know"""
 
@@ -15,22 +15,39 @@ def das_huggingface_pirater():
     dataset = load_dataset('multi_nli')
     columns = ['premise', 'hypothesis', 'genre', 'label']
 
-    print('Saving as csv\'s... ')
-    for dset in dataset.keys():
-        # Weird bug here that adds 'unnamed 0 index kinda row to the final product, plss fix
-        pd.DataFrame(list(zip(*[dataset[dset][i] for i in columns])), columns=columns).to_csv(f'data/raw/{dset}.csv')
+    if save_format == 'pd':
+        print('Saving as csv\'s... ')
+        for dset in dataset.keys():
+            # Weird bug here that adds 'unnamed 0 index kinda row to the final product, plss fix
+            pd.DataFrame(list(zip(*[dataset[dset][i] for i in columns])), columns=columns).to_csv(f'data/raw/{dset}.csv')
 
-def dataset_preprocessor(data_path):
-    data = pd.read_csv(f'data/raw/{data_path}.csv')
-    tokenizer = get_tokenizer('basic_english')
+    elif save_format == 'huggingface':
+        save(dataset, 'data/raw/multi_nli_dataset.pt')
 
-    data[['premise', 'hypothesis']] = data[['premise', 'hypothesis']].astype(str).map(tokenizer)
+def dataset_preprocessor(data_path, train_test_split=0.9):
+    # from torch.utils.data import DataLoader
+    # data = pd.read_csv(f'data/raw/{data_path}.csv')
+    # tokenizer = get_tokenizer('basic_english')
 
-    combined_with_delim = [['[CLS]'] + i + ['[SEP]'] + r + ['[SEP]']  for i, r in zip(data['premise'], data['hypothesis'])]
+    # # data[['premise', 'hypothesis']] = data[['premise', 'hypothesis']].astype(str).map(tokenizer)
 
-    save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
-    save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
-    save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
+    # data = data.loc[:, ['premise', 'hypothesis', 'label']]
+    # dat_lod  = DataLoader(data.values, batch_size=16, collate_fn=collate_fn, shuffle=False)
+    # next(iter(dat_lod))
+    # combined_with_delim = [['[CLS]'] + i + ['[SEP]'] + r + ['[SEP]']  for i, r in zip(data['premise'], data['hypothesis'])]
+
+    # save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
+    # save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
+    # save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
+    data = pd.read_csv(data_path)
+    train_set_size = int(len(data))*0.9 
+    train_set = data.loc[:train_set_size]
+    test_set = data.loc[train_set_size:]
+
+    pd.to_csv(train_set)
+    pd.to_csv(test_set)
+
+    # If anything, this funciton should be something that automatically maps sentences to tokens to save time during training?
     
 def make_extended_vocab(data_path, model_name='bert'):
     """
--- hypo_prediction/train_model.py
+++ hypo_prediction/train_model.py
@@ -1,22 +1,49 @@
+from transformers import Trainer, TrainingArguments 
 from torch.utils.data import DataLoader 
-from torch import load
+from torch import load, tensor
+import pandas as pd
 
+def make_dataloader(data_path, tokenizer_path, batch_size=16, train=True):
+    data = pd.read_csv({data_path})[['hypothesis', 'premise', 'label']].values
 
-def make_dataloader(data_path, tokenizer_path):
-    dat_input = load(f'{data_path}-input.pt')
-    dat_targets = load(f'{data_path}-targets.pt')
+    tokenizer = load(tokenizer_path) # Should be huggingface tokenizer class!
+
+    def collate_fn(input):
+        hypo, pred, lab = ([i[r] for i in input] for r in range(3))
+        output = tokenizer(text=hypo, text_pair=pred, padding="max_length", truncation=True, return_tensors='pt')
+        output['labels'] = tensor(lab)
+        return output
+
+    data_loader = DataLoader(dataset=data, batch_size=batch_size, collate_fn=collate_fn, shuffle=train)
+
+    return data_loader
+
+def make_huggingface_dataset(data_path, tokenizer_path):
     tokenizer = load(tokenizer_path)
+    data = load(data_path).values
+
+    def tokenize_function(input):
+        return tokenizer()
+    
+    
+
+
+    tokenizer = load(tokenizer_path) # Should be huggingface tokenizer class!
 
 
 
+def train_model(model, train_data, val_data, tokenizer):
 
-    data_loader = DataLoader()
+    train_path = 'data/raw/train.csv'
+    val_path = 'data/raw/validation_matched.csv'
+    tokenizer_path = 'models/bert-base-uncased_extended_tokenizer.pt'
 
-def tokenize_function(examples, tokenizer):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+    train_loader = make_dataloader(train_path, tokenizer_path)
+    train_loader = make_dataloader(val_path, tokenizer_path, train=False)
+    
 
-# def train_model(model, data, tokenizer):
 
 
 if __name__ == "__main__":
-    make_dataloader('data/processed/train', 'models/bert-base-uncased_extended_tokenizer.pt')
+    make_dataloader('data/processed/train', 'models/bert-base-uncased_extended_tokenizer.pt', train=True)
+    make_dataloader('data/processed/train', 'models/bert-base-uncased_extended_tokenizer.pt', train=False)
