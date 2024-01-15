8d323764cedbf24ed15ec4f15e3a320caf203386
TheGoldenChicken <Karl Meisner>
2024-01-15 15:56:33 +0100

    add working data pirater

--- hypo_prediction/data/make_dataset.py
+++ hypo_prediction/data/make_dataset.py
@@ -18,62 +18,63 @@ def das_huggingface_pirater(save_format):
     if save_format == 'pd':
         print('Saving as csv\'s... ')
         for dset in dataset.keys():
-            # Weird bug here that adds 'unnamed 0 index kinda row to the final product, plss fix
             pd.DataFrame(list(zip(*[dataset[dset][i] for i in columns])), columns=columns).to_csv(f'data/raw/{dset}.csv')
 
     elif save_format == 'huggingface':
         save(dataset, 'data/raw/multi_nli_dataset.pt')
 
-def dataset_preprocessor(data_path, train_test_split=0.9):
-    # from torch.utils.data import DataLoader
-    # data = pd.read_csv(f'data/raw/{data_path}.csv')
-    # tokenizer = get_tokenizer('basic_english')
+def dataset_preprocessor(data_path, tokenizer_path, train_test_split=0.9):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    # tokenizer = load(tokenizer_path)
+    data = load(data_path)
 
-    # # data[['premise', 'hypothesis']] = data[['premise', 'hypothesis']].astype(str).map(tokenizer)
+    def tokenize_function(input):
+        return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
 
-    # data = data.loc[:, ['premise', 'hypothesis', 'label']]
-    # dat_lod  = DataLoader(data.values, batch_size=16, collate_fn=collate_fn, shuffle=False)
-    # next(iter(dat_lod))
-    # combined_with_delim = [['[CLS]'] + i + ['[SEP]'] + r + ['[SEP]']  for i, r in zip(data['premise'], data['hypothesis'])]
+#     # save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
+#     # save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
+#     # save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
+#     data = pd.read_csv(data_path)
+#     train_set_size = int(len(data))*0.9 
+#     train_set = data.loc[:train_set_size]
+#     test_set = data.loc[train_set_size:]
+#     train_set.to_csv('data/raw/train_set.csv')
+#     test_set.to_csv('data/raw/test_set.csv')
+    for st in data.keys():
+        save(data[st].map(tokenize_function), f'data/processed/tokenized_{st}.pt') 
 
-    # save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
-    # save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
-    # save(list(data['genre']), f'./data/processed/{data_path}-genre.pt')
-    data = pd.read_csv(data_path)
-    train_set_size = int(len(data))*0.9 
-    train_set = data.loc[:train_set_size]
-    test_set = data.loc[train_set_size:]
-    train_set.to_csv('data/raw/train_set.csv')
-    test_set.to_csv('data/raw/test_set.csv')
+    # tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
-    # If anything, this funciton should be something that automatically maps sentences to tokens to save time during training?
-    
+        
 def make_extended_vocab(data_path, model_name='bert'):
     """
     Assume 'data' is file loadable by pytorch that is in some way a list of lists of tokenized words  
     """
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     data = load(data_path)
-    new_words = list(set(itertools.chain(*data)))
-    tokenizer.add_tokens(new_words)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = tokenizer.train_new_from_iterator(data['train']['premise'] + data['train']['hypothesis'], vocab_size=90000)
+    tokenizer.save_pretrained(f'models/{model_name}-extended-tokenizer')
 
-    save(tokenizer, f'models/{model_name}_extended_tokenizer.pt')
+    # save(tokenizer, f'models/{model_name}_extended_tokenizer.pt')
+    # pre_tokenizer = get_tokenizer('basic_english')
+    # tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    # data = load(data_path)
+    # new_words = set(itertools.chain(*list(map(pre_tokenizer, data['train']['hypothesis']))))
+    # new_words = new_words.union(set(itertools.chain(*list(map(pre_tokenizer, data['train']['premise'])))))
+    # new_words = list(new_words)
+    # tokenizer.add_tokens(new_words)
+    # tokenizer.save(f'models/{model_name}_extended_tokenizer.json')
+    # save(tokenizer, f'models/{model_name}_extended_tokenizer.pt')
 
  
 if __name__ == '__main__':
     # All datasets in the huggingface multi_nli library
-    sets = ['train', 'validation_matched', 'validation_mismatched']
-    das_huggingface_pirater('huggingface')
-    # if not all([os.path.exists(f'./data/raw/{i}.csv') for i in sets]):
-    #     print('Not all raw datasets present, pirating all again')
-    #     das_huggingface_pirater()
+    # das_huggingface_pirater('huggingface')
         
-    # print('Processing data...')
-    # for name in sets:
-    #     dataset_preprocessor(name)
-
     model_name = 'bert-base-uncased'
-    data_path = './data/processed/train-input.pt'
+    data_path = './data/raw/multi_nli_dataset.pt'
     print(f'Extending tokenizer for {model_name} with words from {data_path}')
     make_extended_vocab(data_path=data_path, model_name=model_name)
+    
+    print('Processing data...')
+    dataset_preprocessor('./data/raw/multi_nli_dataset.pt', 'models/bert-base-uncased-extended-tokenizer')
--- hypo_prediction/train_model.py
+++ hypo_prediction/train_model.py
@@ -1,9 +1,9 @@
-from transformers import Trainer, TrainingArguments, BertTokenizerFast
+from transformers import Trainer, TrainingArguments, BertTokenizerFast, AutoTokenizer
 from torch.utils.data import DataLoader 
 from torch import load, tensor
 import pandas as pd
 import evaluate
-
+import numpy as np
 # def make_dataloader(data_path, tokenizer_path, batch_size=16, train=True):
 #     data = pd.read_csv({data_path})[['hypothesis', 'premise', 'label']].values
 
@@ -19,47 +19,48 @@ import evaluate
 
 #     return data_loader
 
-def make_huggingface_dataset(data_path, tokenizer_path):
-    tokenizer = load(tokenizer_path)
-    data = load(data_path)
+# def make_huggingface_dataset(data_path, tokenizer_path):
+#     tokenizer = load(tokenizer_path)
+#     data = load(data_path)
 
-    tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+#     tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
-    def tokenize_function(input):
-        return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
+#     def tokenize_function(input):
+#         return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
     
-    train_set = data['train'].map(tokenize_function)
-    eval_set = data['validation_matched']
+#     train_set = data['train'].map(tokenize_function)
+#     eval_set = data['validation_matched']
 
-    return train_set, eval_set
+#     return train_set, eval_set
 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
 
 
 def train_model(model, data_path, tokenizer_path, train_test=0.9):
     training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
     metric = evaluate.load("accuracy")
-    dataset, eval_set = make_huggingface_dataset(data_path=data_path, tokenizer_path=tokenizer_path)
+    tokenizer = AutoTokenizer.from_pretrained('models/bert-base-uncased-extended')
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
 
-    train_amount = int(len(dataset) * train_test) 
 
-    train_set = dataset[:train_amount]
-    test_set = dataset[train_amount:]
+    train_set = load('data/processed/tokenized_train.pt')
+    eval_set = load('data/processed/tokenized_validation_matched.pt')
+
+
 
     from transformers import BertForSequenceClassification, AdamW
 
     # model = bert-base-uncased
     model = BertForSequenceClassification.from_pretrained(model, num_labels=3)
-    optimizer = AdamW(optimizer_grouped_parameters, lr=2e-5)
-
+    # optimizer = AdamW(optimizer_grouped_parameters, lr=2e-5)
+    model.resize_token_embeddings(len(tokenizer))
     trainer = Trainer(
         model=model,
         args=training_args,
         train_dataset=train_set,
-        eval_dataset=test_set,
+        eval_dataset=eval_set,
         compute_metrics=compute_metrics,
     )
 
