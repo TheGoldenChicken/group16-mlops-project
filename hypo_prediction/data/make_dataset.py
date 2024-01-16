89d2e908933d896213c101ae675a51fd26fb9b1d
TheGoldenChicken <Karl Meisner>
2024-01-16 09:31:25 +0100

    implement torch lightning stuff

--- hypo_prediction/data/make_dataset.py
+++ hypo_prediction/data/make_dataset.py
@@ -3,33 +3,59 @@ import itertools
 import pandas as pd
 from torch import load, save
 from datasets import load_dataset
-from torchtext.data import get_tokenizer
-from transformers import AutoTokenizer
+# from torchtext.data import get_tokenizer
+# from transformers import AutoTokenizer
+import numpy as np
+import torch
+
+def data_preprocessor():
+    data = load_dataset('fashion_mnist')
+    
+    # Repeat for both test and train...
+    for st in data.keys():
+        inputs = torch.tensor(np.array(data[st]['image'])).float()
+        labels = torch.tensor(np.array(data[st]['label']))
+
+        inputs = torch.nn.functional.normalize(inputs)
+        
+        tensor_datset = torch.utils.data.TensorDataset(inputs, labels)
+
+        torch.save(tensor_datset, f'data/processed/{st}.pt')
+
+# def create_dataloader(dataset: torch.utils.TensorDataset):
+#     """
+#     dataset: torch.utils.data.TensorDataset
+#     """
+
+#     # Might not be necessary now that I think of it
+    # pass
 
 
-def das_huggingface_pirater(save_format):
-    """Pirates the huggingface library
-    Yeah no it doesn't really pirate it, but you know"""
 
-    print('Loading multi_nli dataset...')
-    dataset = load_dataset('multi_nli')
-    columns = ['premise', 'hypothesis', 'genre', 'label']
 
-    if save_format == 'pd':
-        print('Saving as csv\'s... ')
-        for dset in dataset.keys():
-            pd.DataFrame(list(zip(*[dataset[dset][i] for i in columns])), columns=columns).to_csv(f'data/raw/{dset}.csv')
+# def das_huggingface_pirater(save_format):
+#     """Pirates the huggingface library
+#     Yeah no it doesn't really pirate it, but you know"""
 
-    elif save_format == 'huggingface':
-        save(dataset, 'data/raw/multi_nli_dataset.pt')
+#     print('Loading multi_nli dataset...')
+#     dataset = load_dataset('multi_nli')
+#     columns = ['premise', 'hypothesis', 'genre', 'label']
 
-def dataset_preprocessor(data_path, tokenizer_path, train_test_split=0.9):
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-    # tokenizer = load(tokenizer_path)
-    data = load(data_path)
+#     if save_format == 'pd':
+#         print('Saving as csv\'s... ')
+#         for dset in dataset.keys():
+#             pd.DataFrame(list(zip(*[dataset[dset][i] for i in columns])), columns=columns).to_csv(f'data/raw/{dset}.csv')
 
-    def tokenize_function(input):
-        return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
+#     elif save_format == 'huggingface':
+#         save(dataset, 'data/raw/multi_nli_dataset.pt')
+
+# def dataset_preprocessor(data_path, tokenizer_path, train_test_split=0.9):
+#     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+#     # tokenizer = load(tokenizer_path)
+#     data = load(data_path)
+
+#     def tokenize_function(input):
+#         return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
 
 #     # save(combined_with_delim, f'./data/processed/{data_path}-input.pt')
 #     # save(list(data['label']), f'./data/processed/{data_path}-targets.pt')
@@ -40,20 +66,20 @@ def dataset_preprocessor(data_path, tokenizer_path, train_test_split=0.9):
 #     test_set = data.loc[train_set_size:]
 #     train_set.to_csv('data/raw/train_set.csv')
 #     test_set.to_csv('data/raw/test_set.csv')
-    for st in data.keys():
-        save(data[st].map(tokenize_function), f'data/processed/tokenized_{st}.pt') 
+    # for st in data.keys():
+    #     save(data[st].map(tokenize_function), f'data/processed/tokenized_{st}.pt') 
 
     # tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
         
-def make_extended_vocab(data_path, model_name='bert'):
-    """
-    Assume 'data' is file loadable by pytorch that is in some way a list of lists of tokenized words  
-    """
-    data = load(data_path)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer = tokenizer.train_new_from_iterator(data['train']['premise'] + data['train']['hypothesis'], vocab_size=90000)
-    tokenizer.save_pretrained(f'models/{model_name}-extended-tokenizer')
+# def make_extended_vocab(data_path, model_name='bert'):
+#     """
+#     Assume 'data' is file loadable by pytorch that is in some way a list of lists of tokenized words  
+#     """
+#     data = load(data_path)
+#     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+#     tokenizer = tokenizer.train_new_from_iterator(data['train']['premise'] + data['train']['hypothesis'], vocab_size=90000)
+#     tokenizer.save_pretrained(f'models/{model_name}-extended-tokenizer')
 
     # save(tokenizer, f'models/{model_name}_extended_tokenizer.pt')
     # pre_tokenizer = get_tokenizer('basic_english')
@@ -68,13 +94,14 @@ def make_extended_vocab(data_path, model_name='bert'):
 
  
 if __name__ == '__main__':
+    data_preprocessor()
     # All datasets in the huggingface multi_nli library
     # das_huggingface_pirater('huggingface')
         
-    model_name = 'bert-base-uncased'
-    data_path = './data/raw/multi_nli_dataset.pt'
-    print(f'Extending tokenizer for {model_name} with words from {data_path}')
-    make_extended_vocab(data_path=data_path, model_name=model_name)
+    # model_name = 'bert-base-uncased'
+    # data_path = './data/raw/multi_nli_dataset.pt'
+    # print(f'Extending tokenizer for {model_name} with words from {data_path}')
+    # make_extended_vocab(data_path=data_path, model_name=model_name)
     
-    print('Processing data...')
-    dataset_preprocessor('./data/raw/multi_nli_dataset.pt', 'models/bert-base-uncased-extended-tokenizer')
+    # print('Processing data...')
+    # dataset_preprocessor('./data/raw/multi_nli_dataset.pt', 'models/bert-base-uncased-extended-tokenizer')
--- hypo_prediction/train_model.py
+++ hypo_prediction/train_model.py
@@ -1,72 +1,40 @@
-from transformers import Trainer, TrainingArguments, BertTokenizerFast, AutoTokenizer
 from torch.utils.data import DataLoader 
 from torch import load, tensor
 import pandas as pd
-import evaluate
 import numpy as np
-# def make_dataloader(data_path, tokenizer_path, batch_size=16, train=True):
-#     data = pd.read_csv({data_path})[['hypothesis', 'premise', 'label']].values
-
-#     tokenizer = load(tokenizer_path) # Should be huggingface tokenizer class!
-
-#     def collate_fn(input):
-#         hypo, pred, lab = ([i[r] for i in input] for r in range(3))
-#         output = tokenizer(text=hypo, text_pair=pred, padding="max_length", truncation=True, return_tensors='pt')
-#         output['labels'] = tensor(lab)
-#         return output
-
-#     data_loader = DataLoader(dataset=data, batch_size=batch_size, collate_fn=collate_fn, shuffle=train)
-
-#     return data_loader
-
-# def make_huggingface_dataset(data_path, tokenizer_path):
-#     tokenizer = load(tokenizer_path)
-#     data = load(data_path)
-
-#     tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
-
-#     def tokenize_function(input):
-#         return tokenizer(text=input['premise'], text_pair=input['hypothesis'], padding="max_length", truncation=True)
-    
-#     train_set = data['train'].map(tokenize_function)
-#     eval_set = data['validation_matched']
-
-#     return train_set, eval_set
+import torch
+import lightning as L
+from torchmetrics.functional import accuracy
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.loggers import CSVLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torch.utils.data import DataLoader, random_split
+from torchmetrics.functional import accuracy
+from lightning.pytorch.callbacks import LearningRateMonitor
+from models import IronManWhenHeIsStruckByThorInThatAvengersMovieNotTheSecondObviouslyTheFirst
 
 
+def train_model():
+    train_data = torch.load('data/processed/train.pt')
+    test_data = torch.load('data/processed/test.pt')
 
-def train_model(model, data_path, tokenizer_path, train_test=0.9):
-    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
-    metric = evaluate.load("accuracy")
-    tokenizer = AutoTokenizer.from_pretrained('models/bert-base-uncased-extended')
-    def compute_metrics(eval_pred):
-        logits, labels = eval_pred
-        predictions = np.argmax(logits, axis=-1)
-        return metric.compute(predictions=predictions, references=labels)
+    train_dataloader = DataLoader(train_data, batch_size=16)
+    test_dataloader = DataLoader(test_data, batch_size=16)
 
+    model = IronManWhenHeIsStruckByThorInThatAvengersMovieNotTheSecondObviouslyTheFirst(lr=0.005)
 
-    train_set = load('data/processed/tokenized_train.pt')
-    eval_set = load('data/processed/tokenized_validation_matched.pt')
-
-
-
-    from transformers import BertForSequenceClassification, AdamW
-
-    # model = bert-base-uncased
-    model = BertForSequenceClassification.from_pretrained(model, num_labels=3)
-    # optimizer = AdamW(optimizer_grouped_parameters, lr=2e-5)
-    model.resize_token_embeddings(len(tokenizer))
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_set,
-        eval_dataset=eval_set,
-        compute_metrics=compute_metrics,
+    trainer = L.Trainer(
+        max_epochs=5,
+        accelerator="auto",
+        devices=1,
+        logger=CSVLogger(save_dir="logs/"),
+        callbacks=[LearningRateMonitor(logging_interval="step")],
     )
 
-    trainer.train()
-
+    trainer.fit(model, train_dataloader, val_dataloaders=None)
+    trainer.test(model, test_dataloader)
 
 
 if __name__ == "__main__":
-    train_model('bert-base-uncased', data_path='data/raw/multi_nli_dataset.pt' ,tokenizer_path='models/bert-base-uncased_extended_tokenizer.pt', train_test=0.9)
+    train_model()
